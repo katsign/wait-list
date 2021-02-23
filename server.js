04a4da387f4b9aff7ec3010455733771b503d5a3
Delta9k8b <keaton.brewster@gmail.com>
2021-02-22 19:42:14 -0600

    init

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ var tables = [
         name: "Kat Ruth",
         phoneNum: "816-222-2222",
         email: "kat@kat.com",
-        uniqueId: "0220202101" 
+        uniqueId: "0220202101"
     },
     {
         name: "Matt Jay",
         phoneNum: "816-111-1111",
         email: "matt@matt.com",
-        uniqueId: "0220202102" 
+        uniqueId: "0220202102"
     }
 ];
 
@@ -36,50 +36,53 @@ var waitlist = [{
 
 
 // Index/Home View
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Tables View
-app.get('/tables', function(req, res) {
+app.get('/tables', function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Reserve View
-app.get('/reserve', function(req, res) {
+app.get('/reserve', function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+
+
+
 // Tables Route
-app.get('/api/tables', function(req, res) {
+app.get('/api/tables', function (req, res) {
     res.json(tables);
 });
 
 // Waitlist Route
-app.get('/api/waitlist', function(req, res) {
+app.get('/api/waitlist', function (req, res) {
     res.json(waitlist);
 });
 
 
-app.delete('/api/tables/:uniqueID', function(req, res) {
-    
+app.delete('/api/tables/:uniqueID', function (req, res) {
+
     const id = req.params.uniqueID;
     for (let i = 0; i < tables.length; i++) {
         if (id === tables[i].uniqueId) {
             tables.splice(i, 1)
-            
+
         }
     }
     res.end();
 })
 
-app.delete('/api/waitlist/:uniqueID', function(req, res) {
-    
+app.delete('/api/waitlist/:uniqueID', function (req, res) {
+
     const id = req.params.uniqueID;
     for (let i = 0; i < waitlist.length; i++) {
         if (id === waitlist[i].uniqueId) {
             waitlist.splice(i, 1)
-            
+
         }
     }
     res.end();
@@ -87,18 +90,18 @@ app.delete('/api/waitlist/:uniqueID', function(req, res) {
 
 
 // Delete All
-app.delete('/api/waitlist', function(req, res){
+app.delete('/api/waitlist', function (req, res) {
     waitlist = [];
     res.end();
 });
 
-app.delete('/api/tables', function(req, res){
+app.delete('/api/tables', function (req, res) {
     tables = [];
     res.end();
 });
 
 // Post New Table
-app.post('/api/tables', function(req, res) {
+app.post('/api/tables', function (req, res) {
     if (tables.length < 3) {
         tables.push(req.body);
         res.send("tables");
@@ -111,6 +114,6 @@ app.post('/api/tables', function(req, res) {
 
 
 // Listener...
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`Listening on Port ${PORT}`);
 });
